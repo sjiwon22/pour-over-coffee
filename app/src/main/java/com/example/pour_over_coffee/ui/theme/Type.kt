3ec8094fa428e2465e247d10ad0d1dcdd485f9b7
Jiwon Seo <sjiwon22@gmail.com>
2025-06-12 23:09:38 +0900

    Set Consolas font

--- app/src/main/java/com/example/pour_over_coffee/ui/theme/Type.kt
+++ app/src/main/java/com/example/pour_over_coffee/ui/theme/Type.kt
@@ -1,34 +1,31 @@
 package com.example.pour_over_coffee.ui.theme
 
+import android.graphics.Typeface
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Cursive,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+private val Consolas = FontFamily(Typeface.create("Consolas", Typeface.NORMAL))
+
+// Set of Material typography styles to start with, all using Consolas
+val Typography = androidx.compose.material3.Typography().run {
+    copy(
+        displayLarge = displayLarge.copy(fontFamily = Consolas),
+        displayMedium = displayMedium.copy(fontFamily = Consolas),
+        displaySmall = displaySmall.copy(fontFamily = Consolas),
+        headlineLarge = headlineLarge.copy(fontFamily = Consolas),
+        headlineMedium = headlineMedium.copy(fontFamily = Consolas),
+        headlineSmall = headlineSmall.copy(fontFamily = Consolas),
+        titleLarge = titleLarge.copy(fontFamily = Consolas),
+        titleMedium = titleMedium.copy(fontFamily = Consolas),
+        titleSmall = titleSmall.copy(fontFamily = Consolas),
+        bodyLarge = bodyLarge.copy(fontFamily = Consolas),
+        bodyMedium = bodyMedium.copy(fontFamily = Consolas),
+        bodySmall = bodySmall.copy(fontFamily = Consolas),
+        labelLarge = labelLarge.copy(fontFamily = Consolas),
+        labelMedium = labelMedium.copy(fontFamily = Consolas),
+        labelSmall = labelSmall.copy(fontFamily = Consolas),
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
-)
+}
